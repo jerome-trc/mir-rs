15c0c54fb3c972f4b81a0e781734e2d17ed360b6
jerome-trc <circus.of.rats@outlook.com>
2023-08-25 17:44:13 -0400

    Sys: provide a feature flag over `MIR_PARALLEL_GEN`

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # mir-rs
 
+## About
+
 The `mircomp-sys` crate is a set of auto-generated FFI bindings for [Vladimir Makarov's MIR](https://github.com/vnmakarov/mir) toolchain.
 
 The `mircomp` crate is a safe Rust wrapper for `mircomp-sys` to allow more ergonomic use by other Rust projects.
 
 Please note that neither this project nor Makarov's are related to Rust's [Mid-level IR](https://rustc-dev-guide.rust-lang.org/mir/index.html).
+
+## Feature Flags
+
+`mircomp-sys` comes with the `parallel` feature flag, which passes along the `MIR_PARALLEL_GEN` preprocessor setting, enabling parallelized generation of machine code.
 
 ## Licensing
 
--- sys/build.rs
+++ sys/build.rs
@@ -1,57 +1,57 @@
 use std::path::PathBuf;
 
 fn main() -> miette::Result<(), Box<dyn std::error::Error>> {
-    let repo_path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "mir"].iter().collect();
+	let repo_path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "mir"].iter().collect();
 
-    if !repo_path.exists() {
-        return Err(err_msg(format!(
-            "Repository not found at: `{}`",
-            repo_path.display()
-        )));
-    }
+	if !repo_path.exists() {
+		return Err(err_msg(format!(
+			"Repository not found at: `{}`",
+			repo_path.display()
+		)));
+	}
 
 	let mut build = cc::Build::new();
 
 	if std::env::var("DEBUG").unwrap() == "false" {
-        build.define("NDEBUG", "1");
-    }
+		build.define("NDEBUG", "1");
+	}
 
-    if std::env::var("CARGO_FEATURE_PARALLEL_GEN").is_ok() {
-        build.define("MIR_PARALLEL_GEN", "1");
-    }
+	if std::env::var("CARGO_FEATURE_PARALLEL").is_ok() {
+		build.define("MIR_PARALLEL_GEN", "1");
+	}
 
-    build
-        .flag("-std=gnu11")
-        .flag("-Wno-all")
+	build
+		.flag("-std=gnu11")
+		.flag("-Wno-all")
 		.flag("-Wno-extra")
 		.flag("-Wno-abi")
-        .flag("-fsigned-char")
-        .flag_if_supported("-fno-tree-sra")
-        .flag_if_supported("-fno-ipa-cp-clone")
-        .include(&repo_path)
-        .file(repo_path.join("mir.c"))
-        .file(repo_path.join("mir-gen.c"))
-        .file(repo_path.join("c2mir/c2mir.c"))
-        .compile("mir");
+		.flag("-fsigned-char")
+		.flag_if_supported("-fno-tree-sra")
+		.flag_if_supported("-fno-ipa-cp-clone")
+		.include(&repo_path)
+		.file(repo_path.join("mir.c"))
+		.file(repo_path.join("mir-gen.c"))
+		.file(repo_path.join("c2mir/c2mir.c"))
+		.compile("mir");
 
-    println!("cargo:rustc-link-lib=static=mir");
+	println!("cargo:rustc-link-lib=static=mir");
 
-    bindgen::Builder::default()
+	bindgen::Builder::default()
 		.clang_arg(format!("-I{}", repo_path.display()))
 		.header(path_string(repo_path.join("mir.h")))
-        .header(path_string(repo_path.join("mir-gen.h")))
-        .header(path_string(repo_path.join("c2mir/c2mir.h")))
+		.header(path_string(repo_path.join("mir-gen.h")))
+		.header(path_string(repo_path.join("c2mir/c2mir.h")))
 		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()?
-        .write_to_file(PathBuf::from(std::env::var("OUT_DIR")?).join("bindings.rs"))?;
+		.generate()?
+		.write_to_file(PathBuf::from(std::env::var("OUT_DIR")?).join("bindings.rs"))?;
 
-    Ok(())
+	Ok(())
 }
 
 /// Used to reduce the number of noisy turbofishes that need to be written.
 #[must_use]
 fn err_msg(msg: String) -> Box<dyn std::error::Error> {
-    Box::from(msg)
+	Box::from(msg)
 }
 
 /// Used for [`bindgen::Builder::header`] arguments.
