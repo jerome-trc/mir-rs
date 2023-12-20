f0ca49b6afb442dd330b8e8607540d37452fff46
jerome-trc <circus.of.rats@outlook.com>
2023-12-20 09:10:34 -0500

    Basics of wrapper type for `MIR_context_`

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,58 @@
 //!
 //! [Vladimir Makarov's MIR toolchain]: https://github.com/vnmakarov/mir
 //! [Mid-level IR]: https://rustc-dev-guide.rust-lang.org/mir/index.html
+
+/// Fully re-entrant state implicit to any usage of the MIR toolchain.
+#[derive(Debug)]
+pub struct Context {
+	inner: sys::MIR_context_t,
+	gen_count: u32,
+}
+
+impl Context {
+	/// Note that MIR forces the following:
+	/// - if the `parallel` feature flag is disabled,
+	/// `generators` will always be set to 1.
+	/// - `generators` is zero, it will be silently set to 1.
+	#[must_use]
+	pub fn new(generators: u32) -> Self {
+		unsafe {
+			let inner = sys::_MIR_init();
+			sys::MIR_gen_init(inner, generators as i32);
+
+			Self {
+				inner,
+				gen_count: generators,
+			}
+		}
+	}
+
+	/// How many code generators was this context initialized with?
+	#[must_use]
+	pub fn generator_count(&self) -> u32 {
+		self.gen_count
+	}
+}
+
+impl Drop for Context {
+	fn drop(&mut self) {
+		unsafe {
+			sys::MIR_finish(self.inner);
+		}
+	}
+}
+
+unsafe impl Send for Context {}
+
+#[cfg(feature = "parallel")]
+unsafe impl Sync for Context {}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn context_smoke() {
+		let _ = Context::new(1);
+	}
+}
